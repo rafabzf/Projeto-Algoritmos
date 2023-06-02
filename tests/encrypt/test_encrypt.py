651f60ab739442f2384dbdff4a93b1fcfd6afb91
Rafael <rafinhabzf@gmail.com>
2023-06-02 18:00:58 -0300

    Requisito 2

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,18 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError):
+        encrypt_message('string', 'string')
+
+    with pytest.raises(TypeError):
+        encrypt_message(120, 100)
+
+    assert encrypt_message('String', 0) == 'gnirtS'
+    assert encrypt_message('String', -4) == 'gnirtS'
+    assert encrypt_message('String', 10) == 'gnirtS'
+    assert encrypt_message('String', 11) == 'gnirtS'
+    assert encrypt_message('String', 3) == 'rtS_gni'
+    assert encrypt_message('String', 4) == 'gn_irtS'
