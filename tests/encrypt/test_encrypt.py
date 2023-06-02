cb51fbbc54b05556b87bf153e0d5fd6bbf64549c
Rafael <rafinhabzf@gmail.com>
2023-06-02 18:04:19 -0300

    Correção de lint

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -2,7 +2,6 @@ from challenges.challenge_encrypt_message import encrypt_message
 import pytest
 
 
-
 def test_encrypt_message():
     with pytest.raises(TypeError):
         encrypt_message('string', 'string')
