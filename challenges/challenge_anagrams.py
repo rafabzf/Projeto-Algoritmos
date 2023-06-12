9c69d766994f37be6ee96ee5268a62f55b5d6bfa
Rafael <rafinhabzf@gmail.com>
2023-06-11 23:21:32 -0300

    Corrigindo req 4

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,13 +1,22 @@
 def is_anagram(first_string, second_string):
     word1 = list(first_string.lower())
     word2 = list(second_string.lower())
-
     sort_word(word1)
     sort_word(word2)
     if word1 != word2 or len(first_string) == 0 or len(second_string) == 0:
-        return (''.join(word1), ''.join(word2), False)
+        return ("".join(word1), "".join(word2), False)
     else:
-        return (''.join(word1), ''.join(word2), True)
+        return ("".join(word1), "".join(word2), True)
+
+
+def sort_word(word, start=0, end=None):
+    if end is None:
+        end = len(word)
+    if (end - start) > 1:
+        middle = (start + end) // 2
+        sort_word(word, start, middle)
+        sort_word(word, middle, end)
+        merge(word, start, middle, end)
 
 
 def merge(word, start, middle, end):
@@ -19,10 +28,10 @@ def merge(word, start, middle, end):
         if l_i >= len(left):
             word[index] = r[r_i]
             r_i = r_i + 1
-        elif left[l_i] < r[r_i]:
+        elif r_i >= len(r):
             word[index] = left[l_i]
             l_i = l_i + 1
-        elif r_i >= len(r):
+        elif left[l_i] < r[r_i]:
             word[index] = left[l_i]
             l_i = l_i + 1
         else:
@@ -30,14 +39,4 @@ def merge(word, start, middle, end):
             r_i = r_i + 1
 
 
-def sort_word(word, start=0, end=None):
-    if end is None:
-        end == len(word)
-    if (end - start) > 1:
-        middle = (start + end) // 2
-        sort_word(word, start, middle)
-        sort_word(word, middle, end)
-        sort_word(word, start, middle, end)
-
-
-print(is_anagram('', ''))
+print(is_anagram("", ""))
